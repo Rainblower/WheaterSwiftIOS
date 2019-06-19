f7dc1a9bab779cb177bafc2ae0ab58761f6c69a9
Rainblower <red5757687@bk.ru>
2019-06-19 02:06:17 -0700

    Initial Commit

---
+++ WheaterSwiftIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WheaterSwiftIOS
+//
+//  Created by WSR on 19/06/2019.
+//  Copyright © 2019 WSR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
